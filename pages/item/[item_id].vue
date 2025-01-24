9057f7b3fbc10c28cb32ed40296d464c59196f9c
Festralus <kezarnus@gmail.com>
2025-01-24 06:59:06 +0300

    item page layout progress

--- pages/item/[item_id].vue
+++ pages/item/[item_id].vue
@@ -1,5 +1,60 @@
 <template>
-  <div></div>
+  <div class="item__path">
+    <div class="item__where-from">Home</div>
+    <div class="item__where-from">Shop</div>
+    <div class="item__where-from">Men</div>
+    <div class="item__where-from">T-shirts</div>
+  </div>
+  <div class="item__gallery">
+    <div class="item__main-picture"></div>
+    <div class="item__secondary-picture"></div>
+    <div class="item__secondary-picture"></div>
+    <div class="item__secondary-picture"></div>
+  </div>
+  <div class="item__name">One Life Graphic T-shirt</div>
+  <div class="item__rating">
+    <div class="item__rating-stars">4.5 звездочки</div>
+    <div class="item__rating-number">4.5/5</div>
+  </div>
+  <div class="item__price">
+    <div class="item__price-current">$260</div>
+    <div class="item__price-old">$300</div>
+    <div class="item__discount">-40%</div>
+  </div>
+  <div class="item__description">
+    This graphic t-shirt which is perfect for any occasion. Crafted from a soft
+    and breathable fabric, it offers superior comfort and style.
+  </div>
+  <div class="horizontal-separator"></div>
+  <div class="item__colors-container">
+    <div class="item__colors-title">Select Colors</div>
+    <div class="item__colors-button">O</div>
+    <div class="item__colors-button">O</div>
+    <div class="item__colors-button">O</div>
+  </div>
+  <div class="horizontal-separator"></div>
+  <div class="item__size-container">
+    <div class="item__size-title"></div>
+    <div class="item__size-button">Small</div>
+    <div class="item__size-button">Medium</div>
+    <div class="item__size-button">Large</div>
+    <div class="item__size-button">X-Large</div>
+  </div>
+  <div class="horizontal-separator"></div>
+  <div class="item__cart-container">
+    <div class="item__cart-quantity">
+      <div class="item__cart-quantity--minus">-</div>
+      <div class="item__cart-quantity--number">1</div>
+      <div class="item__cart-quantity--plus">+</div>
+    </div>
+    <div class="item__cart-button">Add to Cart</div>
+  </div>
+
+  <div class="item__tabs-menu">
+    <div class="item__details-tab">Product Destails</div>
+    <div class="item__reviews-tab">Rating & Reviews</div>
+    <div class="item__reviews-FAQ">FAQs</div>
+  </div>
 </template>
 <script setup></script>
 <style scoped></style>
