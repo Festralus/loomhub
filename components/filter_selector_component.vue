43601b9fc6dc214ccde5260f788fa22c22ceaac9
Festralus <kezarnus@gmail.com>
2025-03-06 22:23:39 +0300

    filter selector component progress

---
+++ components/filter_selector_component.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="selector__title">
+      <div class="selector__title__text"></div>
+      <div class="selector__title__arrow"></div>
+    </div>
+    <div class="selector__filter">
+      <div class="selector__filter__text"></div>
+    </div>
+  </div>
+</template>
+<script setup>
+import ArrowIcon from '@/assets/icons/ArrowIcon.vue';
+
+defineProps({
+  products: {
+    type: Array,
+    required: true,
+  },
+});
+
+onMounted(() => {
+  //   watch(products, () => {
+  //     console.log(products);
+  //   });
+});
+</script>
+<style scoped></style>
