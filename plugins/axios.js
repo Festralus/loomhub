cd56d8973252b11e5327af9c2171b47b69e55618
Festralus <kezarnus@gmail.com>
2025-04-27 20:41:19 +0300

    test

--- plugins/axios.js
+++ plugins/axios.js
@@ -4,7 +4,9 @@ export default defineNuxtPlugin(() => {
   const config = useRuntimeConfig();
 
   const api = axios.create({
-    baseURL: config.public.apiBase,
+    baseURL:
+      config.public.apiBase ||
+      'https://loomhub-backend-production.up.railway.app',
   });
 
   return {
