07e2cb5302962151268c41373ea510682aefad06
Festralus <kezarnus@gmail.com>
2025-06-08 14:36:41 +0300

    chore: fixed typos in footer stories 2

--- data/stories.js
+++ data/stories.js
@@ -6,9 +6,9 @@ const stories = [
     slug: 'about-me',
     path: '/footer/about-me',
     paragraphs: [
-      'Hello! My name is Andrew and I am an inspired and highly motivated web-developer specializing in front-end technologies.',
+      'Hello! My name is Andrey. I am an passionate and highly motivated web developer specializing in front-end technologies.',
       'I really like coding and my main goals are, first and foremost, to do what I enjoy and to enjoy what I do. Also, to see the results of my work.',
-      'In life I am a person who is consistent, responsible, hard-working and punctual. In professional life I am adaptable, approachable, easygoing and open to feedback, a person who respects subordination, able to listen carefully, and I am not afraid to ask and clarify things.',
+      'In life I am a person who is consistent, organized, responsible, hard-working and punctual. In professional life I am adaptable, approachable, systematic, easygoing and open to feedback, a person who respects subordination, able to listen carefully, and I am not afraid to ask and clarify things.',
     ],
   },
   // 2
@@ -17,9 +17,9 @@ const stories = [
     slug: 'skills-development',
     path: '/footer/skills-development',
     paragraphs: [
-      'I firstly enjoyed something related to programming back when I was a kid, 9 or 10 years old. I already was into computers and games, and my good childhood friend found a program that could execute code within any application. We were spending hours upon hours trying to make it perform certain actions in games to cheat our way through otherwise tedious grinds. The program itself was very basic and simple - for example, it could identify the color of a chosen pixel and based on that press some key or move or click a mouse. But in certain games it was more than sufficient to automate processes that could last several hours.',
-      'As my passion for programs and games never went away, gradually my skills of automating processes improved. A while later I began to add scripts, macros and similar features.',
-      'My first experience of web-design was way later, when I was studying in uni. During my second year there we had a course of Information Science, where the main task was to build a simple website using only HTML and CSS. We were free to choose the topic and design, requirements were quite vague. I ended up creating a news resource with several pages, image maps (to navigate users to the correct pages based on the image coordinates they clicked), tables (to store images, text and date of the news) and such. Also I talked my way into using a little bit of jQuery to add drop-down menus!',
+      'I firstly enjoyed something related to programming back when I was a kid, 9 or 10 years old. I already was into computers and games, and my good childhood friend found a program that could execute code within any application. We were spending hours upon hours trying to make it perform certain actions in games to cheat our way through otherwise tedious grinds. The program itself was very basic and simple - for example, it could identify the color of a chosen pixel and, based on that, press some key or move and click a mouse button. In certain games it was more than sufficient to automate processes that could take several hours.',
+      'As my passion for programs and games never went away, gradually my skills of automating processes improved. A while later I began to add scripts, macros and similar features to my everyday tasks.',
+      'My first experience of web-design was way later, when I was studying in university. During my second year there we had a course of Information Science, where the main task for a semester was to build a simple website using only HTML and CSS. We were free to choose the topic and design, requirements were quite vague. I ended up creating a news resource with several pages, image maps (to navigate users to the correct pages based on the image coordinates they clicked), tables (to store images, text and date of the news) and so on. Also I talked my way into using a little bit of jQuery to add drop-down menus!',
     ],
   },
   // 3
@@ -28,8 +28,8 @@ const stories = [
     slug: 'career',
     path: '/footer/career',
     paragraphs: [
-      "After finishing university I spent a couple of years trying different things as a freelancer. They weren't related to programming but it was the perfect time to try new things out. Later on I joined a really small Start-up called Gamerthings. At Gamerthings our goal was to start with an E-commerce platform that would eventually grow to social networks and other services. For almost the whole course of me being there I was a sole programmer. Prior to this moment I hadn't had much experience and hadn't worked with a project of this scale. During the development we weren't in a hurry and we valued quality, so I was able to learn via courses and internet resources to then implement what I learned in the project.",
-      "It was an amazing experience and I am so grateful that it went exactly like that. Because I was able to practice a lot, never falling into so-called tutorial hell, and I was able to make mistakes, and then correct them. Now I am aware of many pitfalls that I would never know if I went from courses to some kind of team with a supervisor. I just wouldn't be on a level to make wrong decisions to witness the consequences.",
+      "After finishing university, I spent a couple of years trying different things as a freelancer. They weren't exactly related to programming but it was the perfect time to try new things out. Later, I joined a really small Start-up called Gamerthings. At Gamerthings our goal was to start with an E-commerce platform that would eventually grow to social networks and other services. For almost the whole course of me being there I was a sole programmer. Prior to this moment I hadn't had much experience and hadn't worked with a project of this scale. During the development we weren't in a hurry and we valued quality, so I was able to learn via courses and internet resources to then implement what I learned in the project.",
+      "It was an amazing experience and I am truly grateful that it unfolded the way it did. I was able to practice a lot, never falling into so-called tutorial hell, and I was able to make mistakes, and then correct them. Now I am aware of many pitfalls I might never have encountered if I had gone straight from courses into a team with a supervisor. I just wouldn't be on a level to make wrong decisions to witness the consequences.",
       "After developing the website for a little more than a year the financial situation became tricky and I had to move on. It would be unethical for me to include the work in my portfolio, since the Gamerthings project was never frozen nor abandoned. That's why I decided to create Loomhub. And right now I am open to new ideas and opportunities.",
     ],
   },
